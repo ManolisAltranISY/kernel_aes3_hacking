fbdaab67ec0bfbabc8e33350c9c4203d3cf30c07
ManolisAltranISY <emmanouil.nikolakakis@altran.com>
2018-07-03 10:04:26 +0200

    Optimize extreme test

--- add_all/lkm_debug_extreme_time_measurements/kgpio.c
+++ add_all/lkm_debug_extreme_time_measurements/kgpio.c
@@ -19,7 +19,6 @@ static char   message[64] = {0};           ///< Memory for the string that is pa
 static unsigned int count = 0;
 static unsigned int count2 = 0;
 
-static unsigned int gpioOutput = 8;   /// this one shall create some output to use as an input to pin 10 for testing
 static unsigned int irqNumber;        /// we use this number to link interrupt to gpio pin
 int result = 0;
 static struct timespec ts_last, ts_current, ts_diff;
@@ -30,14 +29,10 @@ static int __init hello_init(void){
 
    getnstimeofday(&ts_last); //last time to be the current time
    ts_diff = timespec_sub(ts_last, ts_last); //initial time difference to 0
-
    gpio_direction_input(gpioTest);
-   gpio_direction_output(gpioOutput, true);
-
 	 printk(KERN_INFO "GPIO_TEST: The button state is currently: %d\n", gpio_get_value(gpioTest));  //testing functionality, although pin is floating
 	 irqNumber = gpio_to_irq(gpioTest);
    result = request_irq(irqNumber, (irq_handler_t) gpio_irq_handler, IRQF_TRIGGER_RISING | IRQF_TRIGGER_FALLING, "test_gpio_handler", NULL);  //we'll see what we can do to trigger on both rising and falling edges...
-
    getnstimeofday(&ts_current); //gets current time as ts_current
    ts_last = ts_current;
 
@@ -50,10 +45,6 @@ static void __exit hello_exit(void){
    free_irq(irqNumber, NULL);               // Free the IRQ number, no *dev_id required in this case
    gpio_unexport(gpioTest);               // Unexport the Button GPIO
    gpio_free(gpioTest);                   // Free the Button GPIO
-
-   gpio_unexport(gpioOutput);               // Unexport the Button GPIO
-   gpio_free(gpioOutput);                   // Free the Button GPIO
-
    printk(KERN_INFO "Goodbye, world. Manolis out!\n");
 }
 
@@ -71,6 +62,7 @@ static irq_handler_t gpio_irq_handler(unsigned int irq, void *dev_id, struct pt_
      getnstimeofday(&ts_current); //gets current time as ts_current
      ts_last = ts_current;
      count = 0;
+     msleep(5000);
    }
    return (irq_handler_t) IRQ_HANDLED;      // Announce that the IRQ has been handled correctly
 }
